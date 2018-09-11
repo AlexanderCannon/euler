cde3783fb9ff0e831f417fbe56586a89a3bc8584
Alexander Cannon <a.m.t.cannon@gmail.com>
2018-09-11 09:12:25 +0100

    feat(2.js): re-added original solution
    my original solution is better as it blows the others out of the water!

--- js/2.js
+++ js/2.js
@@ -1,6 +1,6 @@
 const NS_PER_SEC = 1e9;
 
-//way faster but blocking!
+// faster but blocking!
 {
   const start = process.hrtime();
   let [a, b] = [0, 1],
@@ -11,6 +11,7 @@ const NS_PER_SEC = 1e9;
   }
   const end = process.hrtime(start);
   console.log(c, `done in ${end[0] * NS_PER_SEC + end[1]} nanoseconds`);
+  // 69265 nanoseconds
 }
 function* fibGenerator(number = Infinity) {
   let [a, b] = [0, 1];
@@ -38,4 +39,23 @@ const calculateFibLessThan = number => {
   const result = calculateFibLessThan(4000000);
   const end = process.hrtime(start);
   console.log(result, `done in ${end[0] * NS_PER_SEC + end[1]} nanoseconds`);
+  // 331067 nanoseconds (non-blocking)
+}
+
+{
+  // language features smanguage features!
+  // we should all write go; FP has been disproven!
+  const start = process.hrtime();
+  let a = 1,
+    b = 1,
+    c = 0;
+  while (a < 4000000) {
+    let d = a;
+    a = a + b;
+    b = d;
+    if (a % 2 == 0) c += a;
+  }
+  const end = process.hrtime(start);
+  console.log(c, `done in ${end[0] * NS_PER_SEC + end[1]} nanoseconds`);
+  // 3325
 }
